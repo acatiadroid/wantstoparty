1211c7cbe94470c8bf8c6f9fb24556dc7e6033d2
acatiadroid <cooldudequzons@gmail.com>
2022-06-27 15:43:42 +0100

    relocate error handler

--- wantstoparty/errors.py
+++ wantstoparty/errors.py
@@ -21,3 +21,17 @@ class UnhandledError(Exception):
 
 class MissingFileType(Exception):
     pass
+
+def _handle_errorcode(self, code):
+        if code == 401:
+            raise Unauthorized()
+        elif code == 403:
+            raise Forbidden("Likely caused by being banned or invalid API key.")
+        elif code == 400:
+            raise BadRequest("Either caused by max file size being exceeded (100MB), " \
+                "no file recieved, or other reason.")
+        elif code == 500:
+            raise InternalServerError("An internal server error occured. Try again later.")
+        else:
+            raise UnhandledError(f"An unhandled error occured. Error code: {code}")
+            
