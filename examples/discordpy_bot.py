f9eea483fefd56ca109918ce843c08fd79da4d6a
acatiadroid <cooldudequzons@gmail.com>
2022-06-27 14:50:16 +0100

    Add discord bot example

---
+++ examples/discordpy_bot.py
@@ -0,0 +1,23 @@
+import io
+from discord.ext import commands
+
+# Using async so we don't stop the event loop (blocking)
+from wantstoparty._async import WantsToParty
+
+bot = commands.Bot(command_prefix="???") # intents, etc not relevant for demo
+wtp = WantsToParty(api_key="your api key", subdomain="your subdomain")
+
+@bot.command()
+async def upload(ctx: commands.Context):
+    """Uploads all files provided when running this command."""
+    for attachment in ctx.message.attachments:
+        extension = attachment.filename.split(".")
+        extension = extension[len(extension)-1]     # the file extension. I.E., "png"
+
+        bytes = io.BytesIO(await attachment.read()) # store bytes in BytesIO
+        file = await wtp.upload_from_bytes(bytes, extension)
+    
+        await ctx.send(f'<{file["url"]}>')
+
+
+bot.run("token")
