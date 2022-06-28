b26dd7bef179d5247939b6f8c697a07c114fbe60
acatia <69216256+acatiadroid@users.noreply.github.com>
2022-06-28 10:27:00 +0100

    Add non-async example

---
+++ examples/non-async_use.py
@@ -0,0 +1,16 @@
+from wantstoparty import WantsToParty
+import io
+
+wtp = WantsToParty(api_key="your api key", subdomain="your subdomain")
+
+# upload from local file on disk using path to file.
+file = wtp.upload_from_file("cat.png")
+
+# upload from binary data
+my_html = io.BytesIO(b"<h1>Hello world</h1>")
+file = wtp.upload_from_bytes(my_html, "html") # filetype must be specified as such data isn't
+                                       # provided using BytesIO.
+
+
+# Print the usable URL
+print(file["url"])
