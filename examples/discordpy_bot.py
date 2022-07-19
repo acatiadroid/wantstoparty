17ea6d2c439b08b6b3ed7f27a62d5bf90a5355fa
acatiadroid <cooldudequzons@gmail.com>
2022-07-19 14:03:49 +0100

    add examples

--- examples/discordpy_bot.py
+++ examples/discordpy_bot.py
@@ -2,7 +2,7 @@ import io
 from discord.ext import commands
 
 # Using async so we don't stop the event loop (blocking)
-from wantstoparty._async import WantsToParty
+from wantstoparty._async import WantsToParty, Size, MaxFileSizeExceeded
 
 bot = commands.Bot(command_prefix="???") # intents, etc not relevant for demo
 wtp = WantsToParty(api_key="your api key", subdomain="your subdomain")
@@ -15,8 +15,12 @@ async def upload(ctx: commands.Context):
         extension = extension[len(extension)-1]     # the file extension. I.E., "png"
 
         file_data = io.BytesIO(await attachment.read()) # store bytes in BytesIO
-        file = await wtp.upload_from_bytes(file_data, extension)
-    
+        
+        try:                                                                      # VVVVVVVVVVVVV basically 6MB.
+            file = await wtp.upload_from_bytes(file_data, extension, max_bytes=Size(mb=5, kb=1024))
+        except MaxFileSizeExceeded:
+            return await ctx.send("That file is bigger than 6MB. Too big!")
+
         await ctx.send(f'<{file}>')
 
 
---
+++ examples/max_filesize.py
@@ -0,0 +1,14 @@
+# This is a pre-upload check that let's you specify a limit to how
+# many bytes can be uploaded.
+#
+# This is useful if you have a system which let's uploads user-inputted files.
+# I.E., a form which lets a user upload an image.
+
+from wantstoparty import WantsToParty, Size, MaxFileSizeExceeded
+
+wtp = WantsToParty(api_key="your api key", subdomain="your subdomain")
+
+try:
+    wtp.upload_from_file("cat.png", max_bytes=Size(mb=3)) # Alternately use the kb or b kwargs.
+except MaxFileSizeExceeded:
+    print("woah there that file is too big buddy")
