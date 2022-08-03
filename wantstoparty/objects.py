f9e5915e85259ab488b5a350bfc0876a3b8321ae
acatiadroid <cooldudequzons@gmail.com>
2022-08-03 16:49:12 +0100

    object properties fetch from dict instead of vars

--- wantstoparty/objects.py
+++ wantstoparty/objects.py
@@ -1,12 +1,10 @@
 class User:
     """Represents a wants-to.party user."""
     def __init__(self, data: dict):
-        self.__user_id = data.get("user_id", False)
-        self.__user_name = data.get("user_name", False)
-        self.__email = data.get("email", False)
+        self.data = data
 
     def __str__(self) -> str:
-        return str(self.__user_name)
+        return str(self.data.get("user_id"))
 
     @property
     def user_id(self) -> int:
@@ -14,7 +12,7 @@ class User:
         
         If you're using an oauth session, this is only accessible with the `user.read` scope.
         If you're using your user API key, the `user.read` scope is not necessary."""
-        return self.__user_id
+        return self.data.get("user_id")
     
     @property
     def userid(self) -> int:
@@ -23,7 +21,7 @@ class User:
         
         If you're using an oauth session, this is only accessible with the `user.read` scope.
         If you're using your user API key, the `user.read` scope is not necessary."""
-        return self.__user_id
+        return self.data.get("user_id")
 
     @property
     def user_name(self) -> str:
@@ -31,7 +29,7 @@ class User:
         
         If you're using an oauth session, this is only accessible with the `user.read` scope.
         If you're using your user API key, the `user.read` scope is not necessary."""
-        return self.__user_name
+        return self.data.get("user_name")
 
     @property
     def username(self) -> str:
@@ -40,7 +38,7 @@ class User:
         
         If you're using an oauth session, this is only accessible with the `user.read` scope.
         If you're using your user API key, the `user.read` scope is not necessary."""
-        return self.__user_name
+        return self.data.get("user_name")
 
     @property
     def email(self) -> str:
@@ -48,41 +46,38 @@ class User:
         
         If you're using an oauth session, this is only accessible with the `user.email` scope.
         If you're using your user API key, the `user.email` scope is not necessary."""
-        return self.__email
+        return self.data.get("email")
     
 class File:
     """Represents a wants-to.party file."""
     def __init__(self, data: dict):
-        self.__name: str = data.get("name", None)
-        self.__extension: str = data.get("extension", None)
-        self.__filename: str = data.get("filename", None)
-        self.___uploaded_at: int = data.get("uploaded_at", None)
-        self.__url: str = data.get("url", None)
+        self.data = data
 
     def __str__(self) -> str:
-        return str(self.__url)
+        return str(self.data.get("url"))
 
     @property
     def name(self) -> str:
         """The filename and filetype."""
-        return self.__name
+        return self.data.get("name")
 
     @property
     def extension(self) -> str:
         """The file extension. (does not include the ".")"""
-        return self.__extension
+        return self.data.get("extension")
 
     @property
     def filename(self) -> str:
         """The filename only."""
-        return self.__filename
+        return self.data.get("filename")
 
     @property
     def uploaded_at(self) -> int:
         """The Unix timestamp for when the file was created."""
-        return self.___uploaded_at
+        return self.data.get("uploaded_at")
 
     @property
     def url(self) -> str:
         """The full usable URL of the file."""
-        return self.__url
+        return self.data.get("url")
+        
