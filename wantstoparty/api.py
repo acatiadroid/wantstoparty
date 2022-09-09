679944be89c777dacec21912f3e7c0f6758f925e
acatiadroid <cooldudequzons@gmail.com>
2022-09-09 21:36:43 +0100

    add get file hosted size

--- wantstoparty/api.py
+++ wantstoparty/api.py
@@ -4,7 +4,7 @@ from aiohttp import ClientSession, FormData
 
 from .errors import BadRequest, _handle_errorcode
 from .utils import _format_arguments
-from .objects import File, User, Size
+from .objects import File, SizeLimit, User
 
 class URL:
     BASE = "https://wants-to.party"
@@ -24,7 +24,7 @@ class Request:
         *,
         data: dict,
         url_args: dict = None,
-        max_bytes: Size = None
+        max_bytes: SizeLimit = None
     ):
         key = data.get("key")
         headers = {"Authorization": f"User {key}"}
@@ -54,7 +54,7 @@ class Request:
         *,
         data: dict,
         url_args: dict = None,
-        max_bytes: Size = None
+        max_bytes: SizeLimit = None
     ):
         key = data.get("key")
         headers = {"Authorization": f"User {key}"}
@@ -135,14 +135,6 @@ class Request:
             return True
         
         raise BadRequest("That file does not exist.") # just gonna raise 400 even though its 404.
-    
-    def _get_file_hosted_size(file_id: str, subdomain: str):
-        url = f"https://{subdomain}.wants-to.party/{file_id}"
-
-        b = requests.get(url, stream=True).headers['Content-Length']
-
-        return Size(_total=b)
-
 
 class AsyncRequest:
     async def _post_from_file(
@@ -151,7 +143,7 @@ class AsyncRequest:
         *,
         data: dict,
         url_args: dict = None,
-        max_bytes: Size = None
+        max_bytes: SizeLimit = None
     ):
         key = data.get("key")
         headers = {"Authorization": f"User {key}"}
@@ -177,7 +169,7 @@ class AsyncRequest:
         *,
         data: dict,
         url_args: dict = None,
-        max_bytes: Size = None
+        max_bytes: SizeLimit = None
     ):
         key = data.get("key")
         headers = {"Authorization": f"User {key}"}
@@ -243,4 +235,3 @@ class AsyncRequest:
                 return _handle_errorcode(resp.status, payload["message"])
         
             return True
-    
--- wantstoparty/objects.py
+++ wantstoparty/objects.py
@@ -1,14 +1,15 @@
-from typing import Optional, Union
+import requests
+from typing import Union
+from aiohttp import ClientSession
 
 from .errors import MaxFileSizeExceeded
 
-class Size:
+class SizeLimit:
     """A class that defines a maximum file size for a file.
     
     This transforms B, KB & MB units into the no. of bytes specified.
     
     To specify the maximum number of bytes, use the kwargs.
-
     If the maximum file size is exceeded, the MaxFileSizeExceeded exception is raised.
     
     GB+ units not necessary due to wants-to.party's maximum allowed file size of 100MB."""
@@ -17,18 +18,15 @@ class Size:
         *,
         mb: float = None,
         kb: float = None,
-        b:  float = None,
-        _total: int = None
+        b:  float = None
         ):
         self.mb: Union[float, bool] = mb
         self.kb: Union[float, bool] = kb
         self.b:  Union[float, bool] = b
 
-        self.total: Optional[int] = None
         self.exceeded: bool = False
     
-    def _get_bytes_size(self):
-        """Internal method for calculating total number of bytes."""
+    def _check_bytes(self, all_bytes: int) -> None:
         nbytes = 1024
         b = 0
         kb = 0
@@ -43,19 +41,64 @@ class Size:
         if self.b:
             b = self.b
         
-        self.total = mb + kb + b
-    
-    @staticmethod
-    def _get_hosted_size(file):
-        
+        total_allowed_bytes = mb + kb + b
 
-    def _check_bytes(self, all_bytes: int) -> None:
-        """Internal method for validation of file size."""
-        self._get_bytes_size()
-        
-        if all_bytes > self.total: # file exceeded max allowed filesize
+        if all_bytes > total_allowed_bytes: # file exceeded max allowed filesize
             self.exceeded = True
             raise MaxFileSizeExceeded("This file exceeded the maximum file size limit.")
+
+class FileSize:
+    KBSIZE = 1024
+    MBSIZE = 1048576 # in bytes
+    def __init__(self, _total: int):
+        self._total = _total
+    
+    def __str__(self) -> str:
+        count = self.formatted_bytes()
+        fmt = f"{count['mb']}MB, {count['kb']}KB, {count['b']}B"
+        return str(fmt)
+
+    @property
+    def total_bytes(self) -> int:
+        """The number of bytes present in the file."""
+        return self._total
+
+    @property
+    def total_kb(self) -> int:
+        """The number of kilobytes present in the file."""
+        return self._total / self.KBSIZE
+    
+    @property
+    def total_mb(self) -> int:
+        """The number of megabytes present in the file."""
+        return (self._total / self.KBSIZE) / self.KBSIZE
+    
+    @property
+    def formatted_bytes(self) -> dict:
+        """The number of MB, KB & B in the file.
+
+        This counts bytes in order of largest unit to smallest. (MB to B)
+        If the sum bytes is less than 1 MB, it will calculate number of KB's instead (and so on).
+
+        This method factors in sizes that are on the boundary of the next unit (I.E, 1024 B = 1 KB)
+
+        For example:
+        1586230 B -> 1 MB, 525 KB, 54 B"""
+        temp = self._total
+
+        mb = 0
+        kb = 0
+        b = 0
+
+        while temp > 0:
+            if temp >= self.MBSIZE:
+                mb = temp - self.MBSIZE
+            elif temp >= self.KBSIZE:
+                kb = temp - self.KBSIZE
+            else:
+                b = temp
+        
+        return {"mb": mb, "kb": kb, "b": b}
 
 
 class User:
@@ -142,7 +185,35 @@ class File:
         return self.data.get("url")
 
     @classmethod    
-    def get_size(self):
-        pass
-
+    def get_size(self, file_id: str, subdomain: str) -> FileSize:
+        """Returns a `FileSize` object  a hosted files' size.
         
+        Attributes
+        -----------
+        file_id - The file ID and filetype of the hosted file.
+
+        subdomain - The the subdomain which the file is hosted with.
+        """
+        url = f"https://{subdomain}.wants-to.party/{file_id}"
+
+        bcount = len(requests.get(url, stream=True).headers['Content-Length'])
+
+        return FileSize(bcount)
+    
+    @classmethod
+    async def async_get_size(self, file_id: str, subdomain: str) -> FileSize:
+        """A non-blocking method that returns a `FileSize` object  a hosted files' size.
+        
+        Attributes
+        -----------
+        file_id - The file ID and filetype of the hosted file.
+
+        subdomain - The the subdomain which the file is hosted with.
+        """
+        url = f"https://{subdomain}.wants-to.party/{file_id}"
+
+        async with ClientSession() as s:
+            async with s.get(url) as resp:
+                bcount = len(await resp.read())
+        
+        return FileSize(bcount)
