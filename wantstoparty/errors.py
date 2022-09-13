c24d98b82cbc284f25eb51a7b16331fd5ca1e979
acatia <69216256+acatiadroid@users.noreply.github.com>
2022-09-13 10:09:36 +0100

    add doc strings to exceptions

--- wantstoparty/errors.py
+++ wantstoparty/errors.py
@@ -1,28 +1,33 @@
 class MissingRequiredOauthArguments(Exception):
+    """Raised when there are missing Oauth scopes."""
     pass
 
 class NoScopesProvided(Exception):
+    """Raised when no scopes are provided."""
     pass
 
 class Unauthorized(Exception):
+    """Raised when a 401 error code is returned by wants-to.party."""
     pass
 
 class Forbidden(Exception):
+    """Raised when a 403 error code is returned by wants-to.party."""
     pass
 
 class BadRequest(Exception):
+    """Raised when a 400 error code is returned by wants-to.party."""
     pass
 
 class InternalServerError(Exception):
+    """Raised when a 500 error code is returned by wants-to.party."""
     pass
 
 class UnhandledError(Exception):
-    pass
-
-class MissingFileType(Exception):
+    """Raised when a 401 error code is returned by wants-to.party."""
     pass
 
 class MaxFileSizeExceeded(Exception):
+    """Raised when a file size is larger than the preset size, set by the `SizeLimit` object."""
     pass
 
 def _handle_errorcode(code, message):
